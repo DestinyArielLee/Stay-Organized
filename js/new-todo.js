18069f5792e7a68d33d9342babf152e507754a8e
Jonathan Carpio <101604637+jona70x@users.noreply.github.com>
2023-06-20 13:58:17 -0700

    add function to send a post request and add a todo

--- js/new-todo.js
+++ js/new-todo.js
@@ -4,6 +4,8 @@ const forms = document.querySelectorAll(".needs-validation");
 const usernameDropdown = document.querySelector("#select_username");
 const categoryDropdown = document.querySelector("#select_category");
 const urgencyDropdown = document.querySelector("#select_urgency");
+const deadlineInput = document.querySelector("#input_date");
+const descriptionTextInput = document.querySelector("#text_todo");
 
 async function fetcher(endPoint) {
   const response = await fetch(endPoint);
@@ -11,17 +13,61 @@ async function fetcher(endPoint) {
   return data;
 }
 
-async function populateDropdown(dropdown) {
-  const data = await fetcher("http://localhost:8083/api/users");
-  for (const { id, name } of data) {
-    console.log(id, name);
+function populateDropdown(dropdown, dropdownData) {
+  for (const { id, name } of dropdownData) {
     const option = new Option(name, id);
     dropdown.insertAdjacentElement("beforeend", option);
   }
 }
+async function populateUsers() {
+  const data = await fetcher("http://localhost:8083/api/users");
+  populateDropdown(usernameDropdown, data);
+}
+async function populateCategories() {
+  const data = await fetcher("http://localhost:8083/api/categories");
+  populateDropdown(categoryDropdown, data);
+}
+
+async function addToDo() {
+  const selectedUser = usernameDropdown[usernameDropdown.selectedIndex].value;
+  const selectedCategory =
+    categoryDropdown[categoryDropdown.selectedIndex].value;
+  const selectedUrgency = urgencyDropdown[urgencyDropdown.selectedIndex].value;
+  const deadlineDate = deadlineInput.value;
+  const descriptionText = descriptionTextInput.value;
+
+  // Validation
+  if (
+    !selectedUser ||
+    !selectedCategory ||
+    !selectedUrgency ||
+    !deadlineDate ||
+    !descriptionText
+  )
+    return;
+
+  const toDoData = {
+    userid: selectedUser,
+    category: selectedCategory,
+    description: descriptionText,
+    deadline: deadlineDate,
+    priority: selectedUrgency,
+  };
+
+  const response = await fetch('http://localhost:8083/api/todos', {
+    method: 'POST',
+    headers: {
+      'Content-Type': 'application/json'
+    }, 
+    body: JSON.stringify(toDoData)
+  })
+ const data = await response.json()
+ console.log(data);
+}
 
 window.addEventListener("load", () => {
-  populateDropdown(usernameDropdown);
+  populateUsers();
+  populateCategories();
 });
 
 // Loop over them and prevent submission
@@ -33,21 +79,14 @@ Array.from(forms).forEach((form) => {
         event.preventDefault();
         event.stopPropagation();
       }
-
+      event.preventDefault();
       form.classList.add("was-validated");
+      addToDo();
     },
     false
   );
-  form.addEventListener(
-    "reset",
-    (event) => {
-      if (!form.checkValidity()) {
-        event.preventDefault();
-        event.stopPropagation();
-      }
-
-      form.classList.remove("was-validated");
-    },
-    false
-  );
+  form.addEventListener("reset", () => {
+    form.classList.remove("was-validated");
+    form.reset();
+  });
 });
